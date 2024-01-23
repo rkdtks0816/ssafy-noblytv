6751fe5d74d9915c059cec76a852651a8b5ed4fd
이지호 <jeho1129@naver.com>
2024-01-23 15:12:24 +0900

    #S10P12C103-286 :bug: fix: fixed Create Diary Logic

--- backend/project/src/main/java/BACKEND/project/domain/MedicationDto.java
+++ backend/project/src/main/java/BACKEND/project/domain/MedicationDto.java
@@ -8,7 +8,7 @@ import java.time.LocalTime;
 @Data
 public class MedicationDto {
 
-    @NotBlank(message = "약 이름은 필수입니다.")
+    @NotBlank(message = "약 이름은 필수입니다")
     private String medicine;
 
     private LocalTime medicationTime;

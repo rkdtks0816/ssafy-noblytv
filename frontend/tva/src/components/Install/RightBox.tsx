5e6ad52bbea0d3bf1d1f29075b5cdf62e2eaef50
송강산 <rkdtks0816@naver.com>
2024-01-29 10:23:38 +0900

    #S10P12C103-422feat: chaged css - 오민상

--- frontend/tva/src/components/Install/RightBox.tsx
+++ frontend/tva/src/components/Install/RightBox.tsx
@@ -1,12 +1,14 @@
 import QRCode from 'qrcode.react';
+
 import RightBoxStyle from './styles/RightBoxStyle';
 
 function RightBox() {
-  const Data = 'http://www.asdf.com';
+  const Url = 'http://i10c103.p.ssafy.io:5173/connect-tv';
 
   return (
     <RightBoxStyle>
-      <QRCode value={Data} size={256} level="H" />
+      <QRCode value={Url} size={256} level="H" />
+
       <div>QR 코드를 스마트폰으로 스캔하세요</div>
     </RightBoxStyle>
   );

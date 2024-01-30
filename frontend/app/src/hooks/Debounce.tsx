34d80afaa579dc64fd1e3f90c2e2ecc241d1f2c5
오민상 <flos9537@gmail.com>
2024-01-30 14:14:12 +0900

    #S10P12C103-459 📝 docs: modified comment

--- frontend/app/src/hooks/Debounce.tsx
+++ frontend/app/src/hooks/Debounce.tsx
@@ -2,12 +2,10 @@ import { useState, useEffect } from 'react';
 
 // 입력값이 일정 시간 안정되면 최종값을 반환하는 useDebounce 훅 정의
 function useDebounce<T>(value: T, delay: number): T {
-  // debounceValue 상태 관리.
   const [debounceValue, setDebounceValue] = useState<T>(value);
 
-  // value 값이 변경될 때마다 디바운스 로직 실행.
+  // value 값이 변경될 때마다 디바운스 로직 실행하여 지정된 지연 시간 후에 debounceValue 업데이트.
   useEffect(() => {
-    // 지정된 지연 시간 후에 debounceValue 업데이트.
     const handler = setTimeout(() => {
       setDebounceValue(value);
     }, delay);
@@ -18,7 +16,6 @@ function useDebounce<T>(value: T, delay: number): T {
     };
   }, [value, delay]);
 
-  // 최종적으로 디바운스된 값을 반환.
   return debounceValue;
 }
 

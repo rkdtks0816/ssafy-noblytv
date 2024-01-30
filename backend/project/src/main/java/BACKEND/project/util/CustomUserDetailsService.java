ff66307c508fc81deca11746e63b771bab54a12c
이지호 <jeho1129@naver.com>
2024-01-30 21:04:23 +0900

    #S10P12C103-623 :recycle: refactoring: Solved Family Login Error + Git Name + Email

--- backend/project/src/main/java/BACKEND/project/util/CustomUserDetailsService.java
+++ backend/project/src/main/java/BACKEND/project/util/CustomUserDetailsService.java
@@ -8,7 +8,6 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
 
 @RequiredArgsConstructor

561cd713a36e4bbf63057d33c04801ddb7c057ed
오민상 <flos9537@gmail.com>
2024-01-31 17:06:25 +0900

    #S10P12C103-631 🐛 fix:  code fix

--- frontend/tva/src/App.tsx
+++ frontend/tva/src/App.tsx
@@ -8,29 +8,14 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Overlay />} />
+        <Route path="/" element={<SignInTV />} />
         <Route path="/sign-in" element={<Overlay />} />
+        {/* gymnastics */}
+        <Route path="/gymnastics/start" element={<GymnasticsStart />} />
+        <Route path="/gymnastics" element={<Gymnastics />} />
       </Routes>
     </Router>
   );
 }
 
 export default App;
-=========
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import TvLayout from './pages/TvLayout';
-import InstallPage from './pages/Install/InstallPage';
-// App 컴포넌트 내에서 라우터 설정
-function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<InstallPage />} />
-        <Route path="/tv" element={<TvLayout />} />
-      </Routes>
-    </Router>
-  );
-}
-
-export default App;
->>>>>>>>> Temporary merge branch 2

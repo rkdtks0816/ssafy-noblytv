b26c2db4c711f1d4ee89ba0f8c0fa917fc6b3f6a
오민상 <flos9537@gmail.com>
2024-02-01 15:39:01 +0900

    #S10P12C103-634 ✨ feat: init. quiz page
    Former-commit-id: d242f574074f1d876c32013aef2bebc0decde11c

--- frontend/tva/src/App.tsx
+++ frontend/tva/src/App.tsx
@@ -3,6 +3,9 @@ import SignInTV from './pages/SignInTV/SignInTV';
 import Overlay from './pages/Overlay/Overlay';
 import GymnasticsStart from './pages/gymnastics/GymnasticsStart';
 import Gymnastics from './pages/gymnastics/Gymnastics';
+import Quiz from './pages/Quiz/Quiz';
+import QuizStart from './pages/Quiz/QuizStart';
+
 // App 컴포넌트 내에서 라우터 설정
 function App() {
   return (
@@ -13,6 +16,9 @@ function App() {
         {/* gymnastics */}
         <Route path="/gymnastics/start" element={<GymnasticsStart />} />
         <Route path="/gymnastics" element={<Gymnastics />} />
+        {/* Quiz */}
+        <Route path="/quiz/start" element={<QuizStart />} />
+        <Route path="/quiz" element={<Quiz />} />
       </Routes>
     </Router>
   );

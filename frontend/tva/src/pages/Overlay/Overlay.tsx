49c1381362cac90e0b800839820ce1951e975c5a
오민상 <flos9537@gmail.com>
2024-02-01 12:12:23 +0900

    #S10P12C103-318 ✨ feat: auto show modal
    페이지가 마운트 된 후 7초 뒤 1번 모달, 14초 뒤 2번 모달 등장 21초 뒤에 2번 모달 닫기
    
    
    Former-commit-id: 157eafd9ed58149718549f09d299376508e90da2

--- frontend/tva/src/pages/Overlay/Overlay.tsx
+++ frontend/tva/src/pages/Overlay/Overlay.tsx
@@ -1,5 +1,5 @@
 // Overlay.tsx
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import BgVideo from '../../components/BgVideo/BgVideo';
 import ChildModal from '../../components/ChildModal/ChildModal';
 
@@ -9,6 +9,37 @@ function Overlay() {
   const openModal = (modalId: number) => {
     setActiveModal(prevModal => (prevModal === modalId ? null : modalId));
   };
+
+  useEffect(() => {
+    const timer1 = setTimeout(() => {
+      setActiveModal(1); // 7초 후에 1번 모달 열기
+    }, 7000);
+
+    const timer2 = setTimeout(() => {
+      setActiveModal(null); // 12초 후에 1번 모달 닫기
+    }, 12000);
+
+    const timer3 = setTimeout(() => {
+      setActiveModal(null); // 14초 후에 1번 모달 닫기
+      setActiveModal(2); // 그리고 2번 모달 열기
+    }, 14000);
+
+    const timer4 = setTimeout(() => {
+      setActiveModal(null); // 12초 후에 1번 모달 닫기
+    }, 19000);
+
+    const timer5 = setTimeout(() => {
+      setActiveModal(null); // 21초 후에 2번 모달 닫기
+    }, 21000);
+
+    return () => {
+      clearTimeout(timer1);
+      clearTimeout(timer2);
+      clearTimeout(timer3);
+      clearTimeout(timer4);
+      clearTimeout(timer5);
+    };
+  }, []);
 
   return (
     <div>
@@ -21,7 +52,7 @@ function Overlay() {
       />
       <ChildModal
         title="할아버지!"
-        content="약 아직 안드셨죠?!?!?!?;ㅑ"
+        content="약 아직 안드셨죠?!?!?!?"
         isActive={activeModal === 2}
         onToggle={() => openModal(2)}
       />

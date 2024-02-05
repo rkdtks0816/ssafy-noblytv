fc9ca22e940dc82653d98d4408b48a71ab8852ca
이지호 <jeho1129@naver.com>
2024-02-05 10:55:01 +0900

    #S10P12C103-544 :sparkles: feat: GET Schedule
    Former-commit-id: fe82b57bc732169be558068f6e72b7a8f72ccd27

--- backend/project/src/main/java/BACKEND/project/controller/ScheduleController.java
+++ backend/project/src/main/java/BACKEND/project/controller/ScheduleController.java
@@ -10,6 +10,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/schedule")
@@ -23,5 +25,12 @@ public class ScheduleController {
     public ResponseEntity<Schedule> registerSchedule(@PathVariable("oldUserId") String oldUserId, @RequestBody ScheduleDto scheduleDto) {
         Schedule newSchedule = scheduleService.registerSchedule(oldUserId, scheduleDto);
         return ResponseEntity.status(HttpStatus.CREATED).body(newSchedule);
+    }
+
+    @GetMapping("/{oldUserId}")
+    @Operation(summary = "일정 조회")
+    public ResponseEntity<List<Schedule>> getSchedules(@PathVariable("oldUserId") String oldUserId) {
+        List<Schedule> schedules = scheduleService.getSchedules(oldUserId);
+        return ResponseEntity.ok(schedules);
     }
 }

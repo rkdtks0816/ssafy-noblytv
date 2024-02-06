477e8e424c016ed05807fb64fd1495c99186841f
오민상 <flos9537@gmail.com>
2024-02-07 08:55:36 +0900

    #S10P12C103-664 ✨ feat: Gymnastic motion bug fix and ad news videos
    모드에 따른 영상 출력 변경 완료(찐막)
    
    
    Former-commit-id: 15e9c757637c35fbe499ead066a7062aa3812529

--- frontend/tva/src/components/BgVideo/BgVideo.tsx
+++ frontend/tva/src/components/BgVideo/BgVideo.tsx
@@ -10,15 +10,13 @@ function BgVideo({ muted, currentMode }: BgVideoProps) {
   const [videoSrc, setVideoSrc] = useState('src/assets/news.mp4');
 
   useEffect(() => {
-    let newVideoSrc = '';
     if (currentMode === 'news') {
-      newVideoSrc = 'src/assets/news_closing.mp4';
+      setVideoSrc('src/assets/news_closing.mp4');
     } else if (currentMode === 'commercial') {
-      newVideoSrc = 'src/assets/advertisement_2.mp4';
+      setVideoSrc('src/assets/advertisement_2.mp4');
     } else if (currentMode === 'main') {
-      newVideoSrc = 'src/assets/news.mp4';
+      setVideoSrc('src/assets/news.mp4');
     }
-    setVideoSrc(newVideoSrc);
   }, [currentMode]);
 
   return (

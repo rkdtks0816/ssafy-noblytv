163907e056c5c022d6284ea230b993a8975da03c
오민상 <flos9537@gmail.com>
2024-02-06 17:48:14 +0900

    #S10P12C103-664 ✨ feat: Gymnastic motion bug fix and ad news videos
    뒤도 돌려서 확인중
    
    
    Former-commit-id: 81628c455fba8cea9ea585b1fcfa51bf3965928d

--- frontend/tva/src/pages/Overlay/Overlay.tsx
+++ frontend/tva/src/pages/Overlay/Overlay.tsx
@@ -8,89 +8,42 @@ import useSocket from '../../hooks/useSocket';
 function Overlay() {
   const socket = useSocket('http://i10c103.p.ssafy.io:9000');
   const [activeModal, setActiveModal] = useState<number | null>(null);
-  const [news, setNews] = useState('');
-  const [commercial, setCommercial] = useState('');
-
-  // 디버깅용 코드
-  // useEffect(() => {
-  //   if (socket) {
-  //     const events = ['mode', 'mode_type', 'message'];
-  //     events.forEach(event => {
-  //       socket.on(event, data => {
-  //         console.log(`이벤트 수신 [${event}]: `, data);
-  //       });
-  //     });
-  //     return () => {
-  //       events.forEach(event => {
-  //         socket.off(event);
-  //       });
-  //     };
-  //   }
-  // }, [socket]);
 
   useEffect(() => {
     if (socket) {
-      console.log('소켓 연결 확인', socket);
-      const events = ['mode', 'mode_type', 'message'];
-      events.forEach(event => {
-        socket.on(event, data => {
-          if (event === 'mode') {
-            console.log('소켓 모드', event);
-            switch (
-              data // 'event'를 'data'로 변경
-            ) {
-              case 'gymnastic':
-                setActiveModal(1);
-                break;
-              case 'quiz':
-                setActiveModal(2);
-                break;
-              case 'diary':
-                setActiveModal(3);
-                break;
-              default:
-                setActiveModal(null);
-                break;
-            }
-          } else if (event === 'mode_type') {
-            console.log('소켓 타입 ', event);
-            switch (
-              data // 'event'를 'data'로 변경
-            ) {
-              case 'news':
-                console.log('소켓 타입 ', event);
-                setNews('news');
-                setCommercial('');
-                break;
-              case 'commercial':
-                console.log('소켓 타입 ', event);
-                setNews('');
-                setCommercial('commercial');
-                break;
-              default:
-                console.log('소켓 타입 ', event);
-                setNews('');
-                setCommercial('');
-                break;
-            }
-          }
-        });
+      console.log(socket);
+      socket.on('mode', mode => {
+        // console.log('socket connect', mode);
+        switch (mode) {
+          case 'gymnastic':
+            setActiveModal(1);
+            console.log('activeModal', activeModal);
+            break;
+          case 'quiz':
+            setActiveModal(2);
+            console.log('activeModal', activeModal);
+            break;
+          case 'diary':
+            setActiveModal(3);
+            console.log('activeModal', activeModal);
+            break;
+          default:
+            setActiveModal(null);
+            break;
+        }
       });
     }
 
     return () => {
-      if (socket) {
-        socket.off('mode');
-        socket.off('mode_type');
-      }
+      if (socket) socket.off('mode');
     };
-  }, [activeModal, commercial, news, socket]);
+  }, [activeModal, socket]);
 
   const isMuted = activeModal !== null;
 
   return (
     <>
-      <BgVideo muted={isMuted} news={news} commercial={commercial} />
+      <BgVideo muted={isMuted} news="dsa" commercial="dsa" />
       <div>
         {activeModal === 1 && <GymnasticsModal />}
         {activeModal === 2 && <QuizModal />}

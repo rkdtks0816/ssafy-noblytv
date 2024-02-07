93528c7afa41d6a5eb91609cfc5579ca50a1ba43
오민상 <flos9537@gmail.com>
2024-02-07 17:46:40 +0900

    #S10P12C103-314 ✨ feat: init. selected display (string or url)
    코드 순서 및 일부 로직 변경
    
    
    Former-commit-id: 62fac938ec235fa943250413013c230e3f5be2c7

--- frontend/tva/src/components/ActionModal/FamilyVideoModal.tsx
+++ frontend/tva/src/components/ActionModal/FamilyVideoModal.tsx
@@ -10,19 +10,20 @@ function FamilyVideoModal() {
 
   const socket: Socket | null = useSocket('http://i10c103.p.ssafy.io:9000');
 
+  const isVideoPath = (path: string): boolean =>
+    /^(http(s?):)([/|.|\w|\s|-])*\.(?:mp4|mov)$/.test(path);
+
   useEffect(() => {
     let timer: string | number | NodeJS.Timeout | undefined;
     if (socket) {
       socket.on('message', (data: string) => {
-        // 소켓통신에서 처음 데이터 수신했을 때 모달창 등장시키기
         console.log('Video data received:', data);
         setFamilyVideos(data);
         setIsActive(true);
-        // 영상을 확인하겠다고 했을 때 처리
-        if (data === 'yes') {
-          setIsFullScreen(true);
 
-          // BE에서 영상데이터 정보 가져오기
+        if (isVideoPath(data)) {
+          // 비디오 경로가 확인되었을 때 전체 화면 모드로 전환
+          setIsFullScreen(true);
         } else if (
           data === 'stop' ||
           data === 'no' ||
@@ -45,19 +46,13 @@ function FamilyVideoModal() {
   const toggleModal = () => {
     setIsActive(!isActive);
   };
-  // 서버에서 받은 데이터가 비디오 경로인지 확인하기 위한 함수
-  const isVideoPath = (path: string): boolean =>
-    /^(http(s?):)([/|.|\w|\s|-])*\.(?:mp4|mov)$/.test(path);
 
-  // displayContent 조건 수정
   const displayContent = (() => {
     if (['mute', 'muteoff', 'start', 'stop'].includes(familyVideos)) {
-      return ''; // 이 경우엔 빈 문자열 반환
+      return '';
     }
     if (isVideoPath(familyVideos)) {
-      // 비디오 경로일 경우, video 태그를 통해 비디오를 재생
       return (
-        // 자막 추가해야한다는 접근성 지침. 여기에서는 무시
         // eslint-disable-next-line jsx-a11y/media-has-caption
         <video controls autoPlay>
           <source src={familyVideos} type="video/mp4" />
@@ -65,7 +60,7 @@ function FamilyVideoModal() {
         </video>
       );
     }
-    return familyVideos; // 일반 텍스트일 경우
+    return familyVideos;
   })();
 
   return (
@@ -73,7 +68,7 @@ function FamilyVideoModal() {
       content={displayContent}
       isActive={isActive}
       onToggle={toggleModal}
-      isFullScreen={isFullScreen || false} // 기본값 false 설정
+      isFullScreen={isFullScreen} // 직접 변경된 부분
     />
   );
 }

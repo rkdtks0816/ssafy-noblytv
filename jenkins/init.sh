0d59b91ac04ab9de312276599861650743b97131
송강산 <rkdtks0816@naver.com>
2024-02-14 15:13:52 +0900

    ♻ refactor : file refactor
    Former-commit-id: 8b37dced9944d592f2d2a74cdea3a8bb4dc4d0a7

--- jenkins/init.sh
+++ jenkins/init.sh
@@ -6,24 +6,24 @@ if [ ! -d "$JENKINS_DIR" ]; then
 fi
 
 # Docker Compose 실행
-docker-compose up -d
+sudo docker-compose up -d
 
 # Jenkins 컨테이너가 완전히 실행될 때까지 대기
-sleep 60
+sudo sleep 60
 
 # Jenkins 폴더로 이동
-cd ./jenkins
+sudo cd ./jenkins
 
 # update center에 필요한 CA 파일 다운로드
 UPDATE_CENTER_DIR="./update-center-rootCAs"
 if [ ! -d "$UPDATE_CENTER_DIR" ]; then
-    mkdir "$UPDATE_CENTER_DIR"
+    sudo mkdir "$UPDATE_CENTER_DIR"
 fi
 
-wget https://cdn.jsdelivr.net/gh/lework/jenkins-update-center/rootCA/update-center.crt -O "$UPDATE_CENTER_DIR/update-center.crt"
+sudo wget https://cdn.jsdelivr.net/gh/lework/jenkins-update-center/rootCA/update-center.crt -O "$UPDATE_CENTER_DIR/update-center.crt"
 
 # Jenkins 설정 파일 수정
 sudo sed -i 's#https://updates.jenkins.io/update-center.json#https://raw.githubusercontent.com/lework/jenkins-update-center/master/updates/tencent/update-center.json#' ./hudson.model.UpdateCenter.xml
 
 # Jenkins 재시작 (필수)
-docker restart jenkins
+sudo docker restart jenkins

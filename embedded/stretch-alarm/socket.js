bb03a6d3ffa2d1df678326c64f246b455910463d
현상균 <htts1049@gmail.com>
2024-02-15 10:38:49 +0900

    #S10P12C103-497 ♻️ refactor: Stretch Alarm Refactor
    Former-commit-id: 1696e7db2b2f85f89dd89d9476ecb3a7b886459f

--- embedded/stretch-alarm/socket.js
+++ embedded/stretch-alarm/socket.js
@@ -5,23 +5,28 @@ const sendData = {
   probability: 1.0,
 };
 
-function setData(name, value){
+// Set data to send as {class name, probability}
+function setData(name, value) {
   sendData.class = name;
   sendData.probability = value;
 }
 
+// Connect to server
 socket.on("connect", () => {
   console.log("Connected to server");
 });
 
+// Disconnect to server
 socket.on("disconnect", () => {
   console.log("Disconnected from server");
 });
 
+// Message from server
 socket.on("message", (res) => {
   console.log(res);
 });
 
+// Get distance from ultrasonic sensor, set exist value according to the distance
 socket.on("ultrasonic", (data) => {
   if (data > 0) {
     setExist(true);
